0ed48d490e27f20df67aa91dc2fcb8c5594b210d
Ilya <inshildr@gmail.com>
2026-08-31 14:25:28 +0300

    Перенос в папку Раздел 3

---
+++ Раздел 3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return React.createElement('div', {}, 'проект');
+  // return (
+  //   <div>Проект</div>
+  // );
+}
+
+export default App;
