695fc58bab113452995dc0db8958f41111a7a16d
Ilya <inshildr@gmail.com>
2026-08-31 17:58:04 +0300

    9.2

--- Раздел 3/src/Components/JournalItem/JournalItem.jsx
+++ Раздел 3/src/Components/JournalItem/JournalItem.jsx
@@ -1,12 +1,16 @@
 import './JournalItem.css';
 
 function JournalItem() {
+    const title = 'Подготовка макетов приложения';
+    const date = new Date();
+    const text = 'Надо сделать мобильную и десктопную версию';
+
     return (
         <div className="journal-item">
-            <h2 className="journal-item__header">1</h2>
+            <h2 className="journal-item__header">{title}</h2>
             <h2 className="journal-item__body">
-                <div className="journal-item__data">date</div>
-                <div className="journal-item__text">text</div>
+                <div className="journal-item__date">{date.toDateString()}</div>
+                <div className="journal-item__text">{text}</div>
             </h2>
         </div>
     );
